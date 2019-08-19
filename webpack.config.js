6294b662523ae1c90a59541e4a1afa95dadcde0f
Anastasia Videneeva <nastya.videneeva@gmail.com>
2019-08-19 10:28:43 -0700

    Adds webpack server configuration

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,11 @@ module.exports = {
     filename: `bundle.js`,
     path: path.join(__dirname, `public`)
   },
-  devtool: `source-map`
+  devtool: `source-map`,
+  devServer: {
+    contentBase: path.join(__dirname, `public`),
+    publicPath: `http://localhost:8080/`,
+    compress: true,
+    watchContentBase: true
+  }
 };
