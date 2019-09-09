37c91632883a4e90b181aa449168819d9e64d21c
Anastasia Videneeva <nastya.videneeva@gmail.com>
2019-09-09 10:34:25 -0700

    Adds event types

---
+++ src/data.js
@@ -0,0 +1,11 @@
+const EVENT_TYPES = [`bus`, `check-in`, `drive`, `flight`, `restaurant`, `ship`, `sightseeing`, `taxi`, `train`, `transport`, `trip`];
+
+export const eventData = () => ({
+  type: ``,
+  city: ``,
+  photos: [],
+  description: ``,
+  price: 0,
+  time: 0,
+  date: ``
+});
