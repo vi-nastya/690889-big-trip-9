777e91f9451dd4c54f3abbcbb2f4765fb36dac0e
Anastasia Videneeva <nastya.videneeva@gmail.com>
2019-09-21 15:05:05 -0700

    Adds stats component

---
+++ src/components/stats.js
@@ -0,0 +1,25 @@
+import {AbstractComponent} from '../utils';
+
+export class Stats extends AbstractComponent {
+  constructor() {
+    super();
+  }
+
+  getTemplate() {
+    return `<section class="statistics">
+    <h2>Trip statistics</h2>
+
+    <div class="statistics__item statistics__item--money">
+      <canvas class="statistics__chart  statistics__chart--money" width="900"></canvas>
+    </div>
+
+    <div class="statistics__item statistics__item--transport">
+      <canvas class="statistics__chart  statistics__chart--transport" width="900"></canvas>
+    </div>
+
+    <div class="statistics__item statistics__item--time-spend">
+      <canvas class="statistics__chart  statistics__chart--time" width="900"></canvas>
+    </div>
+  </section>`;
+  }
+}
