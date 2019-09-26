12d8fc8aee2b4a0d20b5d7e2b2ce11f6a6d93f5f
Anastasia Videneeva <nastya.videneeva@gmail.com>
2019-09-26 08:10:41 -0700

    Adds rendering of offer selectors

--- src/components/edit-event-form.js
+++ src/components/edit-event-form.js
@@ -8,6 +8,18 @@ const renderTypeSelector = (type, selectedType) => {
 </div>`;
 };
 
+const renderOfferSelector = (offer, isSelected) => {
+  const formattedTitle = offer.title.split(` `).join(`-`);
+  return `<div class="event__offer-selector">
+  <input class="event__offer-checkbox  visually-hidden" id="event-offer-${formattedTitle}-1" type="checkbox" name="event-offer-${formattedTitle}" ${isSelected ? `checked` : ``}>
+  <label class="event__offer-label" for="event-offer-${formattedTitle}-1">
+    <span class="event__offer-title">${offer.title}</span>
+    &plus;
+    &euro;&nbsp;<span class="event__offer-price">${offer.price}</span>
+  </label>
+</div>`;
+};
+
 export class EventEditForm extends AbstractComponent {
   constructor(eventData) {
     super();
@@ -98,50 +110,7 @@ export class EventEditForm extends AbstractComponent {
                         <h3 class="event__section-title  event__section-title--offers">Offers</h3>
 
                         <div class="event__available-offers">
-                          <div class="event__offer-selector">
-                            <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" checked>
-                            <label class="event__offer-label" for="event-offer-luggage-1">
-                              <span class="event__offer-title">Add luggage</span>
-                              &plus;
-                              &euro;&nbsp;<span class="event__offer-price">30</span>
-                            </label>
-                          </div>
-
-                          <div class="event__offer-selector">
-                            <input class="event__offer-checkbox  visually-hidden" id="event-offer-comfort-1" type="checkbox" name="event-offer-comfort" checked>
-                            <label class="event__offer-label" for="event-offer-comfort-1">
-                              <span class="event__offer-title">Switch to comfort class</span>
-                              &plus;
-                              &euro;&nbsp;<span class="event__offer-price">100</span>
-                            </label>
-                          </div>
-
-                          <div class="event__offer-selector">
-                            <input class="event__offer-checkbox  visually-hidden" id="event-offer-meal-1" type="checkbox" name="event-offer-meal">
-                            <label class="event__offer-label" for="event-offer-meal-1">
-                              <span class="event__offer-title">Add meal</span>
-                              &plus;
-                              &euro;&nbsp;<span class="event__offer-price">15</span>
-                            </label>
-                          </div>
-
-                          <div class="event__offer-selector">
-                            <input class="event__offer-checkbox  visually-hidden" id="event-offer-seats-1" type="checkbox" name="event-offer-seats">
-                            <label class="event__offer-label" for="event-offer-seats-1">
-                              <span class="event__offer-title">Choose seats</span>
-                              &plus;
-                              &euro;&nbsp;<span class="event__offer-price">5</span>
-                            </label>
-                          </div>
-
-                          <div class="event__offer-selector">
-                            <input class="event__offer-checkbox  visually-hidden" id="event-offer-train-1" type="checkbox" name="event-offer-train">
-                            <label class="event__offer-label" for="event-offer-train-1">
-                              <span class="event__offer-title">Travel by train</span>
-                              &plus;
-                              &euro;&nbsp;<span class="event__offer-price">40</span>
-                            </label>
-                          </div>
+                          ${this._options.map((option) => renderOfferSelector(option, option.accepted))}
                         </div>
                       </section>
 
